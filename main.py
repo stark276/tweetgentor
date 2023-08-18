5a5401f2c764175625b569b9dad5da0825f5f1b0
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2023-08-17 23:20:57 -0700

    main added

---
+++ main.py
@@ -0,0 +1,12 @@
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+union = set1 | set2
+intersection = set1 & set2
+difference = set1 - set2
+differenceq = set2 - set1
+
+print(union)
+print(intersection)
+print(difference)
+print(differenceq)
+
